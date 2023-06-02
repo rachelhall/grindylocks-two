e349e5b14fb9a8e8c821aa09512e22ba79b7bc7b
rachelhall <rachelraspberrypie@gmail.com>
2023-06-02 10:50:53 -0700

    add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/app";
 import { api } from "grindylocks/utils/api";
 import "grindylocks/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />;
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
