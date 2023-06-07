7920681cdf84cec4dd763a59e02d61e6d26f2c99
rachelhall <rachelraspberrypie@gmail.com>
2023-06-07 13:29:41 -0700

    Next image tag

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,44 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "grindylocks/server/api/trpc";
+import { clerkClient } from "@clerk/nextjs";
+import { type User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.profileImageUrl,
+  };
+};
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.userId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    console.log({ users });
+
+    return posts.map((post) => {
+      const user = users.find((user) => user.id === post.userId);
+      if (!user)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "User for post not found",
+        });
+
+      return {
+        post,
+        user: { ...user, username: user.username ?? "no username" },
+      };
+    });
   }),
 });
