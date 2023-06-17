123f9ef2215f0adaa3fd9c0eb3ce5edb8d663d94
Rachel Hall <rachelhall@Rachels-MBP.attlocal.net>
2023-06-17 09:55:59 -0700

    editing eslintrc rules

--- src/styleComponents/index.ts
+++ src/styleComponents/index.ts
@@ -3,4 +3,3 @@ export { CloseModalButton } from "./CloseModalButton";
 export { CloseModalButton as CloseButton } from "./CloseModalButton";
 export { Modal } from "./Modal";
 export { Text } from "./Text";
-export { TextInput } from "./TextInput";
